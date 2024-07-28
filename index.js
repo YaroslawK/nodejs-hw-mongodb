0e8df562875e03b71ebea4346cbcbdc59af4057a
Yaroslaw <xxjokerxx933@gmail.com>
2024-07-28 19:48:29 +0300

    hw2

---
+++ index.js
@@ -0,0 +1,9 @@
+import { initMongoConnection } from './src/db/initMongoConnection.js';
+import { startServer } from './src/server.js';
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  startServer();
+};
+
+bootstrap();
---
+++ src/server.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+import pinoPretty from 'pino-pretty';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  const prettyStream = pinoPretty({
+    colorize: true,
+  });
+
+  app.use(
+    pino(
+      {
+        transport: {
+          target: 'pino/file',
+          options: { destination: 1 },
+        },
+      },
+      prettyStream,
+    ),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
+
+  app.get('/contacts', async (req, res, next) => {
+    try {
+      const contacts = await getAllContacts();
+      res.status(200).json({ data: contacts });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.get('/contacts/:contactsId', async (req, res, next) => {
+    try {
+      const { contactsId } = req.params;
+      const contacts = await getContactById(contactsId);
+
+      if (!contacts) {
+        return res.status(404).json({ message: 'Contact not found' });
+      }
+
+      res.status(200).json({ data: contacts });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  app.use('*', (req, res) => {
+    res.status(404).json({ message: 'Not found' });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  return new Promise((resolve, reject) => {
+    app.listen(PORT, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        console.log(`Server is running on port ${PORT}`);
+        resolve();
+      }
+    });
+  });
+};
