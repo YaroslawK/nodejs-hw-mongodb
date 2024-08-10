b4aacbfe53fcace2166132c55e16aacdad705c25
Yaroslaw <xxjokerxx933@gmail.com>
2024-08-10 12:43:03 +0300

    change students routers

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
 import pinoPretty from 'pino-pretty';
+import router from './routers/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -35,44 +35,7 @@ export const startServer = () => {
     });
   });
 
-  app.get('/contacts', async (req, res, next) => {
-    try {
-      const contacts = await getAllContacts();
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully found contacts!',
-        data: contacts,
-      });
-      if (!contacts) {
-        return res
-          .status(404)
-          .json({ status: 'error', message: 'Contacts not found' });
-      }
-    } catch (error) {
-      next(error);
-    }
-  });
-
-  app.get('/contacts/:contactsId', async (req, res, next) => {
-    try {
-      const { contactsId } = req.params;
-      const contacts = await getContactById(contactsId);
-
-      if (!contacts) {
-        return res
-          .status(404)
-          .json({ status: 'error', message: 'Contact not found' });
-      }
-
-      res.status(200).json({
-        status: 200,
-        message: `Successfully found contact with id ${contacts._id}!`,
-        data: contacts,
-      });
-    } catch (error) {
-      next(error);
-    }
-  });
+  app.use(router);
 
   app.use('*', (req, res) => {
     res.status(404).json({ status: 'error', message: 'Not found' });
